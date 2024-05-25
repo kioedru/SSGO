0c0417aa2e16e6246fccfc37bcf42fd7e43b828c
kioedru <2300411008@email.szu.edu.cn>
2024-05-25 10:15:48 +0800

    修改transformer_seq480的微调代码

--- codespace/finetune/transformer_seq480/run_for_five.py
+++ codespace/finetune/transformer_seq480/run_for_five.py
@@ -2,9 +2,13 @@ import subprocess
 
 aspects = ["P", "F", "C"]
 num_class = {"P": 45, "F": 38, "C": 35}
-pretrain_updates = [2, 1, 0]
+# pretrain_updates = [2, 1, 0]
+pretrain_updates = [2]
+
 seeds = [132976111, 1329765519, 1329765522, 1329765525, 1329765529]
-epochs = [100, 200]
+# epochs = [100, 200]
+epochs = [100]
+
 for epoch in epochs:
     for pretrain_update in pretrain_updates:
         for aspect in aspects:
